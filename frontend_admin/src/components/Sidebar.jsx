d9939b250f11b987bbdf7b18fe9736c5aa1617db
MashoodCk <mashoodck77@gmail.com>
2025-03-09 13:15:03 +0530

    sidebar updated

--- frontend_admin/src/components/Sidebar.jsx
+++ frontend_admin/src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
     const menuItems = [
         { item: "Home", path: "/" },
         { item: "Fireman", path: "/fireman" },
+        { item: "Fire Vehicle", path: "/vehicle" }
     ]
 
     return (
@@ -16,7 +17,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
         >
             <div className="flex justify-between items-center p-4">
                 <p className="text-xl font-bold">Menu</p>
-                <CgClose 
+                <CgClose
                     className="text-2xl cursor-pointer"
                     onClick={() => setIsOpen(false)}
                 />
