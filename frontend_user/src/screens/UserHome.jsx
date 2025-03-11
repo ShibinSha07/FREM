79e32f20c2874f5e8d81fbc9bf4140df212a24f9
MashoodCk <mashoodck77@gmail.com>
2025-03-11 21:07:16 +0530

    scroll call button added

--- frontend_user/src/screens/UserHome.jsx
+++ frontend_user/src/screens/UserHome.jsx
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Linking, Button, TouchableOpacity, Image, SafeAreaView, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Linking, TouchableOpacity, SafeAreaView, ActivityIndicator, ScrollView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import axios from 'axios';
 import Layout from '../components/Layout';
 import { API_URL } from '../lib/api.js';
+import { Ionicons } from "@expo/vector-icons";
 
 const UserHome = ({ navigation }) => {
 
     const [location, setLocation] = useState(null);
     const [place, setPlace] = useState("Unknown location")
     const [loading, setLoading] = useState(true);
+
+    const [showFloatingButton, setShowFloatingButton] = useState(false);
+    const scrollViewRef = useRef(null);
 
     useEffect(() => {
         const fetchLocation = async () => {
@@ -74,84 +78,112 @@ const UserHome = ({ navigation }) => {
         }
     };
 
+    const handleScroll = (event) => {
+        const scrollY = event.nativeEvent.contentOffset.y;
+        setShowFloatingButton(scrollY > 600); 
+    };
+
     return (
         <Layout>
-            <View className="flex-1">
-                <View className="w-full h-40 bg-gray-200 rounded-md mb-6 p-4">
-                    <Text className="text-center">Save Your life</Text>
-                </View>
+            <SafeAreaView className="flex-1">
+                <ScrollView
+                    contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }}
+                    showsVerticalScrollIndicator={false}
+                    onScroll={handleScroll}
+                    scrollEventThrottle={16}
+                    ref={scrollViewRef}
+                >
 
-                <View className="w-full h-2/3  rounded-md">
-                    {loading ? (
-                        <View className="flex-1 justify-center items-center bg-gray-200">
-                            <ActivityIndicator size="large" color="#ff5733" />
-                        </View>
-                    ) : (
-                        <>
-                            <View className="w-full h-96 mb-20 bg-white">
-                                <MapView
-                                    style={styles.map}
-                                    initialRegion={{
-                                        latitude: 10.5545,
-                                        longitude: 76.2247,
-                                        latitudeDelta: 0.0922,
-                                        longitudeDelta: 0.0421,
-                                    }}
-                                    region={
-                                        location
-                                            ? {
-                                                latitude: location.coords.latitude,
-                                                longitude: location.coords.longitude,
-                                                latitudeDelta: 0.01,
-                                                longitudeDelta: 0.01,
-                                            }
-                                            : undefined
-                                    }
-                                >
-                                    {location?.coords && (
-                                        <Marker
-                                            coordinate={{
-                                                latitude: location.coords.latitude,
-                                                longitude: location.coords.longitude,
-                                            }}
-                                            title="Your Location"
-                                        />
-                                    )}
-                                </MapView>
+                    <View className="w-full h-40 bg-white rounded-md mb-6 p-4">
+                        <Text className="text-center">Save Your life</Text>
+                    </View>
+
+                    <View className='flex-1 h-[500px] mb-4 rounded-md bg-white p-2'>
+                        {loading ? (
+                            <View className="flex-1 justify-center items-center">
+                                <ActivityIndicator size="large" color="#ff5733" />
                             </View>
+                        ) : (
+                            <View className='flex-1'>
+                                <View className="flex-1 mb-4">
+                                    <MapView
+                                        style={styles.map}
+                                        initialRegion={{
+                                            latitude: 10.5545,
+                                            longitude: 76.2247,
+                                            latitudeDelta: 0.0922,
+                                            longitudeDelta: 0.0421,
+                                        }}
+                                        region={
+                                            location
+                                                ? {
+                                                    latitude: location.coords.latitude,
+                                                    longitude: location.coords.longitude,
+                                                    latitudeDelta: 0.01,
+                                                    longitudeDelta: 0.01,
+                                                }
+                                                : undefined
+                                        }
+                                    >
+                                        {location?.coords && (
+                                            <Marker
+                                                coordinate={{
+                                                    latitude: location.coords.latitude,
+                                                    longitude: location.coords.longitude,
+                                                }}
+                                                title="Your Location"
+                                            />
+                                        )}
+                                    </MapView>
+                                </View>
 
-                            <View className="flex-row justify-between gap-x-2 mb-2">
-                                <TouchableOpacity className="bg-blue-600 py-4 rounded-md flex-1">
-                                    <Text className="text-white text-center text-lg font-bold">
-                                        Nearest Fire Stations
-                                    </Text>
-                                </TouchableOpacity>
+                                <View className="flex-row justify-between gap-x-2 mb-2">
+                                    <TouchableOpacity className="bg-blue-600 py-4 rounded-md flex-1">
+                                        <Text className="text-white text-center text-lg font-bold">
+                                            Nearest Fire Stations
+                                        </Text>
+                                    </TouchableOpacity>
+
+                                    <TouchableOpacity
+                                        className="bg-green-600 py-4 rounded-md flex-1"
+                                        onPress={() => navigation.navigate('Locate')}
+                                    >
+                                        <Text className="text-white text-center text-lg font-bold">
+                                            Locate Fire Vehicle
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
 
                                 <TouchableOpacity
-                                    className="bg-green-600 py-4 rounded-md flex-1"
-                                    onPress={() => navigation.navigate('Locate')}
+                                    className="bg-red-600 py-4 rounded-md"
+                                    onPress={makeCall}
                                 >
                                     <Text className="text-white text-center text-lg font-bold">
-                                        Locate Fire Vehicle
+                                        Emergency Call
                                     </Text>
                                 </TouchableOpacity>
                             </View>
+                        )}
+                    </View>
 
-                            <TouchableOpacity
-                                className="bg-red-600 py-4 rounded-md"
-                                onPress={makeCall}
-                            >
-                                <Text className="text-white text-center text-lg font-bold">
-                                    Emergency Call
-                                </Text>
-                            </TouchableOpacity>
-                        </>
-                    )}
-                </View>
+                    <View>
+                        {Array.from({ length: 50 }).map((_, i) => (
+                            <Text key={i}>Random text</Text>
+                        ))}
+                    </View>
 
+                </ScrollView>
 
+                {!loading && showFloatingButton && (
+                    <TouchableOpacity
+                        className="absolute bottom-6 right-6 bg-red-600 p-4 rounded-full shadow-lg"
+                        onPress={makeCall}
+                    >
+                        <Ionicons name="call" size={28} color="white" />
+                    </TouchableOpacity>
+                )}
 
-            </View>
+            </SafeAreaView>
         </Layout >
     );
 };
