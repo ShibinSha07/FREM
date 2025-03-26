3cdc855fe3d269a56ac31f9e6e4ad5606e4bcb19
MashoodCk <mashoodck77@gmail.com>
2025-03-26 22:29:16 +0530

    supabase postgres branch merged

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/FREM'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:MensHostelGec@db.bvghiufgbxzmjbfyaidt.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
