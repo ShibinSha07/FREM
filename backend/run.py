5cccc054fcb9f1fe5cee7f7fe5d32f4fd67f3430
BassamEC <baschola@gmail.com>
2025-04-07 23:09:32 +0530

    run

--- backend/run.py
+++ backend/run.py
@@ -8,4 +8,4 @@ app = create_app()
 #     print("tables created succesfully")
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host="0.0.0.0", port=3001)
+    socketio.run(app, debug=True, host="0.0.0.0", port=3001, allow_unsafe_werkzeug=True)
