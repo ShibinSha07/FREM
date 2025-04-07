8d6965102e26752da5e2332e115b8099307fad05
BassamEC <baschola@gmail.com>
2025-04-07 23:27:45 +0530

    supabase database

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/FREM'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ilpwrzijwogbymwnajrc:Menshostelgec@aws-0-ap-south-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
